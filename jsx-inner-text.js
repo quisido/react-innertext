29c25b2ff072af094bdf7935f0f2d3a840f73d3a
Charles Stover <charlesstover@charlesstover.com>
2017-12-22 14:25:04 -0600

    jsx-inner-text.js

---
+++ jsx-inner-text.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const innerText = jsx => {
+  if (jsx === null) {
+    return '';
+  }
+  if (typeof jsx === 'number') {
+    return jsx.toString();
+  }
+  if (typeof jsx === 'string') {
+    return jsx;
+  }
+  let text = '';
+  if (
+    jsx.props &&
+    jsx.props.children
+  ) {
+    if (Array.isArray(jsx.props.children)) {
+      const childrenLength = jsx.props.children.length;
+      for (let x = 0; x < childrenLength; ++x) {
+        text += innerText(jsx.props.children[x]);
+      }
+    }
+    else {
+      text += innerText(jsx.props.children);
+    }
+  }
+  return text;
+};
+
+export default innerText;
