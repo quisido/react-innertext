8eb3e48816775c264a5a8b3e7d2071b7f96635e9
Charles Stover <charles.stover@walmart.com>
2018-07-19 17:38:56 -0500

    tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,23 @@
+const path = require('path');
+
 module.exports = {
+  entry: './src/react-innertext.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'react-innertext.js',
+    library: 'react-innertext',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, '.'),
+    umdNamedDefine: true
+  }
 };
