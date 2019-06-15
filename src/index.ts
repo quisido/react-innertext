8db8f089eb96d097422b48a6eb1a73d6de6f7704
Charles Stover <github@charlesstover.com>
2019-06-15 14:50:43 -0700

    converted to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { ReactElement, ReactNode } from 'react';
+
+interface InnerText {
+  (jsx: ReactNode): string;
+  default: InnerText;
+}
+
+const hasProps = (jsx: ReactNode): jsx is ReactElement =>
+  Object.prototype.hasOwnProperty.call(jsx, 'props');
+
+const reduceJsxToString = (previous: string, current: ReactNode): string =>
+  previous + innerText(current);
+
+const innerText: InnerText = (jsx: ReactNode): string => {
+
+  // Empty
+  if (
+    jsx === null ||
+    typeof jsx === 'boolean' ||
+    typeof jsx === 'undefined'
+  ) {
+    return '';
+  }
+
+  // Numeric children.
+  if (typeof jsx === 'number') {
+    return jsx.toString();
+  }
+
+  // String literals.
+  if (typeof jsx === 'string') {
+    return jsx;
+  }
+
+  // Array of JSX.
+  if (Array.isArray(jsx)) {
+    return jsx.reduce<string>(reduceJsxToString, '');
+  }
+
+  // Children prop.
+  if (
+    hasProps(jsx) &&
+    Object.prototype.hasOwnProperty.call(jsx.props, 'children')
+  ) {
+    return innerText(jsx.props.children);
+  }
+
+  // Default
+  return '';
+};
+
+innerText.default = innerText;
+
+export = innerText;
---
+++ tests/index.test.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { expect } from 'chai';
+import innerText from '../index';
+
+interface HasWindow extends NodeJS.Global {
+	window: Window;
+}
+
+const hasWindow = (obj: NodeJS.Global): obj is HasWindow =>
+	Object.prototype.hasOwnProperty.call(obj, 'window');
+
+if (hasWindow(global)) {
+	delete global.window;
+}
+
+describe('react-innertext', (): void => {
+
+	it('should support a window-less environment', (): void => {
+		expect((global as HasWindow).window).to.be.undefined;
+		// @ts-ignore
+		expect(typeof window).to.equal('undefined');
+	});
+
+	it('should support boolean', (): void => {
+		expect(innerText(true)).to.equal('');
+		expect(innerText(false)).to.equal('');
+	});
+
+	it('should support empty objects', (): void => {
+		expect(innerText({})).to.equal('');
+	});
+
+	it('should support null', (): void => {
+		expect(innerText(null)).to.equal('');
+	});
+
+	it('should support numbers', (): void => {
+		expect(innerText(123)).to.equal('123');
+		expect(innerText(420.69)).to.equal('420.69');
+	});
+
+	it('should support undefined', (): void => {
+		expect(innerText(undefined)).to.equal('');
+	});
+
+	it('should support ReactElements', (): void => {
+		expect(innerText(<div>This is a div.</div>)).to.equal('This is a div.');
+	});
+
+	it('should support childless ReactElements', (): void => {
+		expect(innerText(<div />)).to.equal('');
+	});
+
+	it('should support nested ReactElements', (): void => {
+		expect(innerText(<div><p>Paragraph 1.</p><p>Paragraph 2.</p></div>))
+			.to.equal('Paragraph 1.Paragraph 2.');
+	});
+
+	it('should support arrays', (): void => {
+		expect(innerText([
+			null,
+			123,
+			undefined,
+			true,
+			<p>1</p>,
+			[ 'te', 'st' ],
+			420.69,
+			<div><span>2</span><span>three</span><p>4</p></div>
+		])).to.equal('1231test420.692three4');
+	});
+});
